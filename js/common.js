ba64f69addada0fb19676f7e700c93325b542f67
sinnerion <a.dergachow@gmail.com>
2018-03-31 18:27:13 +0300

    added new version of site to dev brunch

---
+++ js/common.js
@@ -0,0 +1,198 @@
+'use strict';
+
+window.onload = function () {
+  if (!Array.from) {
+    Array.from = function () {
+      var toStr = Object.prototype.toString;
+      var isCallable = function isCallable(fn) {
+        return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+      };
+      var toInteger = function toInteger(value) {
+        var number = Number(value);
+        if (isNaN(number)) {
+          return 0;
+        }
+        if (number === 0 || !isFinite(number)) {
+          return number;
+        }
+        return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+      };
+      var maxSafeInteger = Math.pow(2, 53) - 1;
+      var toLength = function toLength(value) {
+        var len = toInteger(value);
+        return Math.min(Math.max(len, 0), maxSafeInteger);
+      };
+
+      // The length property of the from method is 1.
+      return function from(arrayLike /*, mapFn, thisArg */) {
+        // 1. Let C be the this value.
+        var C = this;
+
+        // 2. Let items be ToObject(arrayLike).
+        var items = Object(arrayLike);
+
+        // 3. ReturnIfAbrupt(items).
+        if (arrayLike == null) {
+          throw new TypeError("Array.from requires an array-like object - not null or undefined");
+        }
+
+        // 4. If mapfn is undefined, then let mapping be false.
+        var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+        var T;
+        if (typeof mapFn !== 'undefined') {
+          // 5. else
+          // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+          if (!isCallable(mapFn)) {
+            throw new TypeError('Array.from: when provided, the second argument must be a function');
+          }
+
+          // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+          if (arguments.length > 2) {
+            T = arguments[2];
+          }
+        }
+
+        // 10. Let lenValue be Get(items, "length").
+        // 11. Let len be ToLength(lenValue).
+        var len = toLength(items.length);
+
+        // 13. If IsConstructor(C) is true, then
+        // 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
+        // 14. a. Else, Let A be ArrayCreate(len).
+        var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+
+        // 16. Let k be 0.
+        var k = 0;
+        // 17. Repeat, while k < len… (also steps a - h)
+        var kValue;
+        while (k < len) {
+          kValue = items[k];
+          if (mapFn) {
+            A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+          } else {
+            A[k] = kValue;
+          }
+          k += 1;
+        }
+        // 18. Let putStatus be Put(A, "length", len, true).
+        A.length = len;
+        // 20. Return A.
+        return A;
+      };
+    }();
+  }
+
+  // Empty links prevent default
+  var emptyLinks = document.querySelectorAll('a[href="#"]');
+  Array.from(emptyLinks).forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+    });
+  });
+
+  // Main-nav
+  var mainNavToggle = document.querySelector('.main-nav_toggle');
+  mainNavToggle.addEventListener('click', function () {
+    this.classList.toggle('main-nav_toggle__active');
+    Array.from(this.querySelectorAll('i')).forEach(function (value) {
+      value.classList.toggle('hidden');
+    });
+    document.querySelector('.main-nav').classList.toggle('main-nav__active');
+  });
+
+  // Dynamic age
+  function get_current_age(date) {
+    return (new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000) | 0;
+  }
+
+  Array.from(document.querySelectorAll('.age')).forEach(function (element) {
+    element.innerHTML = get_current_age('1992-04-19');
+  });
+  // Up-btn
+  var upBtn = document.querySelector('.up-btn');
+  var footer = document.querySelector('.footer');
+
+  window.addEventListener('scroll', function upBtnFade() {
+    var currentScroll = this.pageYOffset;
+    if (currentScroll > 200) {
+      footer.style.display = "block";
+    } else {
+      footer.style.display = "none";
+    }
+  });
+  upBtn.addEventListener('click', function () {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  if ('querySelector' in document && 'addEventListener' in window && Array.prototype.forEach) {
+
+    // Function to animate the scroll
+    var smoothScroll = function smoothScroll(anchor, duration) {
+
+      // Calculate how far and how fast to scroll
+      var startLocation = window.pageYOffset;
+      var endLocation = anchor.offsetTop;
+      var distance = endLocation - startLocation;
+      var increments = distance / (duration / 16);
+      var stopAnimation;
+
+      // Scroll the page by an increment, and check if it's time to stop
+      var animateScroll = function animateScroll() {
+        window.scrollBy(0, increments);
+        stopAnimation();
+      };
+
+      // If scrolling down
+      if (increments >= 0) {
+        // Stop animation when you reach the anchor OR the bottom of the page
+        stopAnimation = function stopAnimation() {
+          var travelled = window.pageYOffset;
+          if (travelled >= endLocation - increments || window.innerHeight + travelled >= document.body.offsetHeight) {
+            clearInterval(runAnimation);
+          }
+        };
+      }
+      // If scrolling up
+      else {
+          // Stop animation when you reach the anchor OR the top of the page
+          stopAnimation = function stopAnimation() {
+            var travelled = window.pageYOffset;
+            if (travelled <= (endLocation || 0)) {
+              clearInterval(runAnimation);
+            }
+          };
+        }
+
+      // Loop the animation function
+      var runAnimation = setInterval(animateScroll, 16);
+    };
+
+    // Define smooth scroll links
+    var scrollToggle = document.querySelectorAll('.main-nav a');
+
+    // For each smooth scroll link
+    [].forEach.call(scrollToggle, function (toggle) {
+
+      // When the smooth scroll link is clicked
+      toggle.addEventListener('click', function (e) {
+
+        // Prevent the default link behavior
+        e.preventDefault();
+
+        // Get anchor link and calculate distance from the top
+        var dataID = toggle.getAttribute('href');
+        var dataTarget = document.querySelector(dataID);
+        var dataSpeed = toggle.getAttribute('data-speed');
+
+        // If the anchor exists
+        if (dataTarget) {
+          // Scroll to the anchor
+          smoothScroll(dataTarget, dataSpeed || 500);
+        }
+      }, false);
+    });
+  }
+};
